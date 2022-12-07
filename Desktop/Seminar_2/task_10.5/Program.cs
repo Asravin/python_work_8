df6622ce2ccb764cee91b19c60faec4e32590626
Asravin <maisweedfell@.com>
2022-12-07 20:28:04 +0300

    задача 10 написаная другим способом и дополненны условия

---
+++ Desktop/Seminar_2/task_10.5/Program.cs
@@ -0,0 +1,19 @@
+﻿// Решение задачи №10 с помощью функции "MakeArray"
+
+Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = number.ToString().Length;
+Console.Write(MakeArray(number));
+int MakeArray(int a)
+{
+int result = 0;
+    if (count <3 || count >3)
+    {
+        Console.Write("Введите трехзначное число:");
+    }
+    else
+    {
+        result = ((a  / 10) % 10);
+    }
+return result;
+}
