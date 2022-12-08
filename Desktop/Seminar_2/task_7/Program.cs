b2b026fd42dccedcbe6c6e5f4389ab97f7c016ee
Asravin <maisweedfell@.com>
2022-12-08 19:33:47 +0300

    программа которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.

---
+++ Desktop/Seminar_2/task_7/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = number.ToString().Length;
+int a = number % 10;
+int b = a % 10;
+Console.WriteLine(b);
