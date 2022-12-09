dd3d7e837e9d5376fe07f3f05a3f9169f4f9ff97
Asravin <maisweedfell@.com>
2022-12-09 10:46:41 +0300

    программа которая принимает на вход три числа и выдает максимальное из этих чисел.

---
+++ Desktop/Seminar_2/task_4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Ведите три числа:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+if(a > b && a > c)
+{
+Console.WriteLine(a);
+}
+if(b > a && b > c)
+{
+Console.WriteLine(b);
+}
+if(c > b && c > a)
+{
+Console.WriteLine(c);
+}
