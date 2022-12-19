a822fdb3db351be551b41dbba2fa8ae894bd75ab
Asravin <maisweedfell@.com>
2022-12-19 14:28:53 +0300

    программа  которая будет преобразовывать десятичное число в двоичное.

---
+++ Desktop/Seminar_2/task_42/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+print_binary_view(number);
+void print_binary_view(int N)
+{
+    if(N <= 0) return;
+    print_binary_view(N / 2);
+    Console.Write(N % 2);
+    
+}
