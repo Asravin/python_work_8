7613bcf0edb5707bcfe259e164f3483206b93967
Asravin <maisweedfell@.com>
2022-12-21 16:33:07 +0300

    двумерный массив заполненный случайными числами

---
+++ Desktop/Seminar_2/task_46/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GetRandomMatrix(int rows, int columns, int LeftRange = 0, int RightRange = 10)
+{
+    int[,] matrix = new int[rows, columns];
+    var rand = new Random();
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j ++)
+        {
+            matrix[i, j] = rand.Next(LeftRange, RightRange + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+       for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+         Console.WriteLine();
+        for(int j = 0; j < matrix.GetLength(1); j ++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+    }
+}
+
+int rowsCount = ReadInt("Введите число строк:");
+int columnsCount = ReadInt("Введите число столбцов:");
+int[,] matrix = GetRandomMatrix(rowsCount, columnsCount);
+PrintMatrix(matrix);
