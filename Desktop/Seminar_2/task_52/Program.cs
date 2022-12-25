cf890d36fbf242dc1befbfb69c561b7f3e83dee6
Asravin <maisweedfell@.com>
2022-12-25 16:01:18 +0300

    Найдите среднее арифметическое элементов в каждой строке)

---
+++ Desktop/Seminar_2/task_52/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GetRandomMatrix(int rows, int columns, int LeftRange = 0, int RightRange = 9)
+{
+    int[,] matrix = new int[rows, columns];
+    var rand = new Random();
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j ++)
+        {
+            matrix[i, j] = rand.Next(LeftRange, RightRange + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        double AverageArithmetic =0;
+        for(int j = 0; j < matrix.GetLength(1); j ++)
+        {
+            Console.Write(matrix[i, j] + " ");
+            AverageArithmetic = (AverageArithmetic + matrix[i, j]);
+        }
+         Console.Write($"|||||||среднее арифмитическое строки = {AverageArithmetic / matrix.GetLength(1)}");
+    }
+}
+
+int rowsCount = ReadInt("Введите число строк:");
+int columnsCount = ReadInt("Введите число столбцов:");
+int[,] matrix = GetRandomMatrix(rowsCount, columnsCount);
+PrintMatrix(matrix);
