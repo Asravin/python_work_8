8b59ee7d0d67c5755569de35d8f02c48ba440f01
Asravin <maisweedfell@.com>
2022-12-28 19:30:07 +0300

    программа которая поменяет местами первую и последнюю строку массива.

---
+++ Desktop/Seminar_2/task_53/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+int[,] array = new int[4, 4];
+for(int i = 0; i < array.GetLength(0); i++)
+{
+    for(int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(1, 9);
+        Console.Write(array[i , j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+int temp = 0;
+for(int i = 0; i < array.GetLength(1); i++)
+{
+    temp = array[array.GetLength(0) - 1, i];
+    array[array.GetLength(0) -1, i] = array[0, i];
+    array[0, i] = temp;
+}
+for(int i = 0; i < array.GetLength(0); i++)
+{
+    for(int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write(array[i , j] + " "); 
+    }
+    Console.WriteLine();
+}
