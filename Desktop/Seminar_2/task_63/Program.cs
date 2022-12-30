f5a15c897e4f4ca56a00d121523cd3b6c6e39b69
Asravin <maisweedfell@.com>
2022-12-30 15:25:01 +0300

    программа которая выведет все натуральные числа в промежутке от 1 до N.

---
+++ Desktop/Seminar_2/task_63/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.WriteLine("Введите значение N:");
+int N = Convert.ToInt32(Console.ReadLine());
+int factorial(int n)
+{
+    if(n < 0) return -1;
+    if(n == 1) return 1;
+    Console.Write(factorial(n - 1) + ", ");
+    return n;
+}
+Console.WriteLine(factorial(N));
