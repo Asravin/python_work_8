1b20b1bf9af6700569c21c679ecafe3a3ee87f80
Asravin <maisweedfell@.com>
2022-12-30 18:07:10 +0300

    возведение в степень с помощью рекурсии

---
+++ Desktop/Seminar_2/task_69/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+Console.WriteLine("Введите два числа:");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+int Exponentiation(int a, int b)
+{
+    if(b == 0) return 1;
+    a = a * Exponentiation(a, b - 1);
+    return a;
+}
+Console.WriteLine($"Число {A} в степени {B} = {Exponentiation(A, B)}");
