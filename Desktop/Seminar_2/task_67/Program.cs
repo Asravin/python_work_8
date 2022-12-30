5420b089c981586914cd0900b657d0990f4b41a1
Asravin <maisweedfell@.com>
2022-12-30 17:23:27 +0300

    возврат суммы с помощью рекурсии

---
+++ Desktop/Seminar_2/task_67/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int TheSumOfTheNumber(int sum)
+{
+    if(sum == 0) return 0;
+    else
+    {
+        sum = sum % 10 + TheSumOfTheNumber(sum / 10);
+    }
+    return sum;
+}
+Console.WriteLine("Сумма цифр" + " " + TheSumOfTheNumber(number));
