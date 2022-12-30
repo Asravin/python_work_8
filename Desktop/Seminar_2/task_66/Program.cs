67e5291cd119fe4d8f60ff9e73411a33bdc0c714
Asravin <maisweedfell@.com>
2022-12-30 19:40:13 +0300

    сумма натуральных элементов в факториале

---
+++ Desktop/Seminar_2/task_66/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите два числа:");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+int SumFactorial(int a, int b)
+{
+    if(b == a) return b;
+    return a + SumFactorial(a + 1, b);
+}
+Console.WriteLine("Сумма элементов = " + SumFactorial(A, B) + " в промежутке от " + A + " до " + B);
